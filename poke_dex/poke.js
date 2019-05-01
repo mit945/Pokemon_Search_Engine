7ffea428448c5818ac69c8a2d5b82ff88ee89646
mit945 <mit945.mc@gmail.com>
2019-05-01 14:38:36 -0700

    clean up background color add code

--- poke_dex/poke.js
+++ poke_dex/poke.js
@@ -5,26 +5,26 @@ const baseURL = `https://pokeapi.co/api/v2/`
 const endPoint = `pokemon`
 let titleQuery = $('input[type="text"]').val()
 let pokemonName = "";
- 
+let pokemonTypes = "" ;
 
-
-///loop thru pokemon and give//
-// const 
 
 const queryURL = baseURL + endPoint + '/' + pokemonName 
 
 $(() => {
+
+//////////////////get pokemon names/////////////
 	const pokemonData = $.ajax({
 		url: queryURL 
 		//pokemonName is the name of the pokemon the user is 
 		//searching for 
 	})
-	
+
 const getPokemon = () => {
 	for(let i = 0 ; i < 20 ;i++){
 	$.ajax({
-		url : queryURL
-
+		url : queryURL,
+		dataType : 'json',
+		method: 'GET'
 	
 
 	}).then((pokemonData) => {
@@ -43,26 +43,69 @@ const getPokemon = () => {
 }
 
 
+/////////////////////////////
+///////////////////////////////////////////////////////////////////
+
 
 $('form').on('submit' , (event) => {
 	event.preventDefault()
-	pokemonName = $('input[type="text"]').val()
-	console.log(pokemonName)
-	$.ajax({
-		url :  baseURL + endPoint + '/' + pokemonName
+	// pokemonName = $('input[type="text"]').val()
+	// console.log(pokemonName)
+/////////////types.////////////////////
 
+var types = $('input[type=text]').val().replace(' ','');
+ types = types.split(',');
+ console.log(types)
+
+ var trainerTypes = types.map(function(type){
+ 	console.log(type)
+ 	return $.ajax({
+ 		url: 'http://pokeapi.co/api/v2/type/' + type,
+ 		dataType:'json',
+ 		method: 'GET'
+
+ 	})
+ });
+
+
+
+ 	$.when.apply(null,trainerTypes).then(function(){
+ 		console.log(arguments)
+ 		var pokemonTypes = Array.prototype.slice.call(arguments)//into argument
+
+ 
+ 		getDoubleDmg(pokemonTypes)
+
+ 	})
+
+ });
+
+ const getDoubleDmg = (pokemonTypes) =>{
+		pokemonTypes = pokemonTypes.map((types) => {
+			return types[0].damage_relations.double_damage_from;
+		});
+				// console.log(pokemonTypes)
+}
+
+// console.log(trainerTypes)
+/////////////////////////////////////////
+////////////////////////////////////
+
+
+	// $.ajax({
+	// 	url :  baseURL + endPoint + '/' + pokemonName
 	
 
-	}).then((pokemonData) => {
-		console.log(pokemonData)
-		// $.getJSON(Url, function(data,status){
-		// 	console.
-		// })
-	}),(error) => {
-		console.log(error)
-		console.log(queryURL)
+	// }).then((pokemonData) => {
+	// 	console.log(pokemonData)
+	// 	// $.getJSON(Url, function(data,status){
+	// 	// 	console.
+	// 	// })
+	// }),(error) => {
+	// 	console.log(error)
+	// 	console.log(queryURL)
 
-		}
+	// 	}
 
-	})
+	// })
 })
