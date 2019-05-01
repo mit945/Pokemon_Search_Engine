a4669db7328188da8f3e9a95b4bcd3f0fcc1f531
mit945 <mit945.mc@gmail.com>
2019-05-01 10:08:34 -0700

    sucessfully pull the data and console.log it

--- poke_dex/poke.js
+++ poke_dex/poke.js
@@ -1,40 +1,66 @@
 console.log('hi')
 
 const baseURL = `https://pokeapi.co/api/v2/`
+
 const endPoint = `pokemon`
-let pokemonName;
+let titleQuery = $('input[type="text"]').val()
+let pokemonName = "";
  
 
 
 ///loop thru pokemon and give//
+// const 
 
-		
-
-
-
+const queryURL = baseURL + endPoint + '/' + pokemonName + '/'
 
 $(() => {
-	for(let i = 0 ; i < 20 ;i++){
-	$.ajax({
-		url : baseURL + endPoint + '/' + i + '/'
-	// console.log(queryURL)
-
-	}).then((pokemonData) => {
-		console.log(pokemonData)
-		
-	})
-}
 	const pokemonData = $.ajax({
-		url: queryURL + pokemonName
+		url: queryURL 
 		//pokemonName is the name of the pokemon the user is 
 		//searching for 
 	})
+const getPokemon = () => {
+	for(let i = 0 ; i < 20 ;i++){
+	$.ajax({
+		url : queryURL
+
+	
+
+	}).then((pokemonData) => {
+		console.log(pokemonData)
+		// $.getJSON(Url, function(data,status){
+		// 	console.
+		// })
+	}),(error) => {
+		console.log(error)
+		console.log(queryURL)
+
+		}
+
+	}
+}
+
 
 
 $('form').on('submit' , (event) => {
 	event.preventDefault()
-	titleQuery = $('input[type="text"]').val()
-	console.log(titleQuery)
+	pokemonName = $('input[type="text"]').val()
+	console.log(pokemonName)
+	$.ajax({
+		url :  baseURL + endPoint + '/' + pokemonName
+
+	
+
+	}).then((pokemonData) => {
+		console.log(pokemonData)
+		// $.getJSON(Url, function(data,status){
+		// 	console.
+		// })
+	}),(error) => {
+		console.log(error)
+		console.log(queryURL)
+
+		}
 
 	})
 })
